6f47bf49697c900fad23043255226b88f46ea2dc
schee053 <daniel.scheerooren@wur.nl>
2016-03-10 19:36:42 +0100

    Weekdagen subset gemaakt

--- HypothesesValidation_ESDA_6M.R
+++ HypothesesValidation_ESDA_6M.R
@@ -13,29 +13,14 @@
 # # Aggregate
 # Agg <- aggregate(x = obj, by = list(obj$var1, obj$var2, etc), FUN = mean)
 View(AdamJanuary2013)
-
+#-------------------------------------------------------------------------------------------  
 # Create a list of all charge session datasets
+#-------------------------------------------------------------------------------------------  
 AdamList <- list(AdamJanuary2013, AdamJune2013, AdamNovember2014, AdamJanuary2015, AdamAugust2015, AdamJanuary2016)
 
-# Hyptheses: In the weekend there is less variation in charge locations than during the week.
-WkEnd <- function(x){
-    Adam <- x
-    Adam.Weekends <- subset(Adam, (Adam$Weekday == "Saturday" | Adam$Weekday == "Sunday")) 
-    Adam.Weekdays <- subset(Adam, (Adam$Weekday == "Monday" | Adam$Weekday == "Tuesday"| Adam$Weekday == "Wednesday" | Adam$Weekday == "Thursday"| Adam$Weekday == "Friday"))
-    Weekends.uniq <- unique(unlist(Adam.Weekends$Address))
-    Weekdays.uniq <- unique(unlist(Adam.Weekdays$Address))
-    WndCount <- length(Weekends.uniq)
-    WkCount <-length(Weekdays.uniq)
-    Difference <- length(Weekends.uniq) < length(Weekdays.uniq)
-    Answer <- c(WndCount,WkCount,Difference)
-    return(Answer)
-}
-
-Hypothesis1 <- lapply(AdamList, WkEnd)
-Hypothesis1
-
-#------
+#-------------------------------------------------------------------------------------------  
 # Explore the general values of each dataset.
+#-------------------------------------------------------------------------------------------  
 Mode <- function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
@@ -56,8 +41,28 @@ DatExpl <- function(x){
 
 Exploration <- lapply(AdamList, DatExpl)
 Exploration
+#-------------------------------------------------------------------------------------------  
+# Hyptheses: In the weekend there is less variation in charge locations than during the week.
+#-------------------------------------------------------------------------------------------  
+WkEnd <- function(x){
+    Adam <- x
+    Adam.Weekends <- subset(Adam, (Adam$Weekday == "Saturday" | Adam$Weekday == "Sunday")) 
+    Adam.Weekdays <- subset(Adam, (Adam$Weekday == "Monday" | Adam$Weekday == "Tuesday"| Adam$Weekday == "Wednesday" | Adam$Weekday == "Thursday"| Adam$Weekday == "Friday"))
+    Weekends.uniq <- unique(unlist(Adam.Weekends$Address))
+    Weekdays.uniq <- unique(unlist(Adam.Weekdays$Address))
+    WndCount <- length(Weekends.uniq)
+    WkCount <-length(Weekdays.uniq)
+    Difference <- length(Weekends.uniq) < length(Weekdays.uniq)
+    Answer <- c(WndCount,WkCount,Difference)
+    return(Answer)
+}
 
+Hypothesis1 <- lapply(AdamList, WkEnd)
+Hypothesis1
+
+#-------------------------------------------------------------------------------------------  
 # Observation: At Julianaplein, many different vehicles charge on Tuesdays. (What is there on Tuesdays?)
+#-------------------------------------------------------------------------------------------  
 JulianaPlein <- function(x){
   Plein <- subset(x, x$Address == "Julianaplein 1A 1097DN")
   Day <- Mode(Plein$Weekday)
@@ -68,3 +73,22 @@ Plein <- lapply(AdamList, JulianaPlein)
 Plein
 
 View(AdamJanuary2016)
+
+#-------------------------------------------------------------------------------------------  
+# Subset each weekday
+#-------------------------------------------------------------------------------------------  
+
+Weekday <- function(x){
+  Adam <- x
+  Adam.Monday <- subset(Adam, (Adam$Weekday == "Monday")) 
+  Adam.Tuesday <- subset(Adam, (Adam$Weekday == "Tuesday"))
+  Adam.Wednesday <- subset(Adam, (Adam$Weekday == "Wednesday"))
+  Adam.Thursday <- subset(Adam, (Adam$Weekday == "Thursday"))
+  Adam.Friday <- subset(Adam, (Adam$Weekday == "Friday"))
+  Adam.Saturday <- subset(Adam, (Adam$Weekday == "Saturday"))
+  Adam.Sunday <- subset(Adam, (Adam$Weekday == "Sunday"))
+  Answer <- c(Adam.Monday, Adam.Tuesday, Adam.Wednesday, Adam.Thursday, Adam.Friday, Adam.Saturday, Adam.Sunday)
+  return(Answer)
+}
+
+WeekdayList <- Weekday(AdamList)
